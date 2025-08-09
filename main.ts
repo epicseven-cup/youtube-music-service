ecee88427db4895e7c112e15cd3349489cc14db9
epicsven-cup <jackylin616@gmail.com>
2025-08-08 21:04:20 -0500

    need to rewrite discord rpc for some of the dumb hacks I added

--- main.ts
+++ main.ts
@@ -7,24 +7,22 @@ import * as path from "node:path";
 
 let tray: null | Tray = null
 const service = new YouTubeDiscordRPCService(CLIENT_ID,  5432)
-service.run()
 
-// app.whenReady().then(() => {
-//     // This is a bad selection pathing changing, fix it later
-//     const imagePath = path.join(__dirname, 'tray', 'tray.png');
-//     console.log("imagePath:",imagePath)
-//     let image = nativeImage.createFromPath(imagePath)
-//
-//     image.setTemplateImage(true)
-//     tray = new Tray(image)
-//
-//     tray.setToolTip('Youtube Music Service');
-//     const contextMenu = Menu.buildFromTemplate([
-//         {label: 'Quit', click: () => app.quit()},
-//         {label: 'Restart', click: () => service.restart()},
-//     ])
-//     tray.setContextMenu(contextMenu)
-//
-//     service.run()
-//
-// })
+app.whenReady().then(() => {
+    // This is a bad selection pathing changing, fix it later
+    const imagePath = path.join(__dirname, 'tray', 'tray.png');
+    console.log("imagePath:",imagePath)
+    let image = nativeImage.createFromPath(imagePath)
+
+    image.setTemplateImage(true)
+    tray = new Tray(image)
+
+    tray.setToolTip('Youtube Music Service');
+    const contextMenu = Menu.buildFromTemplate([
+        {label: 'Quit', click: () => app.quit()},
+    ])
+    tray.setContextMenu(contextMenu)
+
+    service.run()
+
+})
